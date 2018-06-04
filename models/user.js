ce827dbc4dae533187366e3b570a4388a2502a75
Henri Koskinen <hkoskinen@protonmail.com>
2018-06-04 23:18:22 +0300

    Add user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  bio: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  location: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  joined: {
+    type: Date,
+    default: Date.now // // `Date.now()` returns the current unix timestamp as a number
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
