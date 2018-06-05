9b5997dc7dc92c81da7c38d491d923e5adabcbe1
Henri Koskinen <hkoskinen@protonmail.com>
2018-06-05 09:12:07 +0300

    Add error route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { TopicsComponent } from './topics/topics.component';
 
+// Note that the order of routes matter!
+// The ** path in the last route is a wildcard. The router will select this
+// route if the requested URL doesn't match any paths for routes defined
+// earlier in the configuration. This is useful for displaying
+// a "404 - Not Found" page or redirecting to another route.
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'topics', component: TopicsComponent }
+  { path: 'topics', component: TopicsComponent },
+  { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
